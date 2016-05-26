4c2c82eb0c6f8148bfeb6be354ffb93ac890261c
JanL <janelle.h.writes@gmail.com>
2016-05-26 10:06:06 -0500

    Initial Commit

---
+++ CalculaTron/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculaTron
+//
+//  Created by joy on 5/26/16.
+//  Copyright © 2016 JanL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculaTron/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalculaTron
+//
+//  Created by joy on 5/26/16.
+//  Copyright © 2016 JanL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

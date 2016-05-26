b4ee79652fc34705421ac87ba32dcc16ecbc0a1e
JanL <janelle.h.writes@gmail.com>
2016-05-26 11:51:25 -0500

    Add commands

--- CalculaTron/ViewController.m
+++ CalculaTron/ViewController.m
@@ -27,6 +27,12 @@
     
 }
 - (IBAction)onOperationTapped:(UIButton *)sender {
+    
+    double op1 = self.operandOne.text.doubleValue;
+    double op2 = self.operandTwo.text.doubleValue;
+    
+    
+    
 }
 
 
